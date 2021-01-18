9fef95c05cbf1c18f4f0403ca3ab2f0bdec326c0
Edison Garcia <egachi@live.com>
2021-01-18 10:46:54 -0600

    Updating

--- api/services/Utils.js
+++ api/services/Utils.js
@@ -4,13 +4,10 @@ module.exports.GetDataSet = async function () {
     var data = require(process.cwd() + '/projects.json');
     return await Project.createEach(data).fetch()
     .catch({ code: 'E_UNIQUE' }, function (err) {
-        res.sendStatus(409);
     })
     .catch({ name: 'UsageError' }, function (err) {
-        res.badRequest();
     })
     .catch(function (err) {
-        res.serverError(err);
     });
 };
 
