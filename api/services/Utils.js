c5bddbaa5c95d6445b8014471478b9c8fdb6a0c6
Edison Garcia <egachi@live.com>
2021-01-17 22:32:11 -0600

    Adding

--- api/services/Utils.js
+++ api/services/Utils.js
@@ -1,4 +1,13 @@
 module.exports.GetDataSet = async function () {
     var data = require(process.cwd() + '/projects.json');
-    return await Project.createEach(data).fetch();
+    return await Project.createEach(data).fetch()
+    .catch({ code: 'E_UNIQUE' }, function (err) {
+        res.sendStatus(409);
+    })
+    .catch({ name: 'UsageError' }, function (err) {
+        res.badRequest();
+    })
+    .catch(function (err) {
+        res.serverError(err);
+    });
 };
